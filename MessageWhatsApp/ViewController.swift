c2a4e86027779c32459f4ae0f15a574f14432a89
Edwy Lugo <edwy.lugo@e-deploy.com.br>
2020-10-04 18:42:24 -0300

    Initial Commit

---
+++ MessageWhatsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessageWhatsApp
+//
+//  Created by Edwy Lugo on 04/10/20.
+//  Copyright © 2020 Edwy Lugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
