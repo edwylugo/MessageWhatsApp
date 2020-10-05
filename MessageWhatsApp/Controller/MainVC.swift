c6b001b2babe7ae84c61dd9301889caadbd9cfe9
Edwy Lugo <edwylugo@gmail.com>
2020-10-05 18:05:56 -0300

    Include alert

--- MessageWhatsApp/Controller/MainVC.swift
+++ MessageWhatsApp/Controller/MainVC.swift
@@ -7,26 +7,29 @@
 //
 
 import UIKit
+import WhatsAppAPI
 
 class MainVC: UIViewController {
     
     var logo: UIImageView = .customImageView(named: "logo")
     
-    let openWhats: UIButton = {
+    let sendMessage: UIButton = {
         let button: UIButton = .customButton()
-        button.setTitle("Abrir Whatsapp", for: .normal)
+        button.setTitle("Send message", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = .boldSystemFont(ofSize: 17)
         return button
     }()
     
-    let sendMessage: UIButton = {
+    let sendMessageSpecific: UIButton = {
         let button: UIButton = .customButton()
-        button.setTitle("Enviar mensagem", for: .normal)
+        button.setTitle("Send message to number specific", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = .boldSystemFont(ofSize: 17)
         return button
     }()
+    
+    let whatsapp = WhatsApp() //Framework
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,8 +39,7 @@ class MainVC: UIViewController {
     }
     
     func addBody() {
-        
-        let stackViewButtons = UIStackView(arrangedSubviews: [UIView(), openWhats, sendMessage,UIView()])
+        let stackViewButtons = UIStackView(arrangedSubviews: [UIView(), sendMessage,sendMessageSpecific,UIView()])
         stackViewButtons.distribution = .fill
         stackViewButtons.axis = .vertical
         stackViewButtons.spacing = 8
@@ -56,17 +58,37 @@ class MainVC: UIViewController {
                                            bottom: 0,
                                            right: 16))
         
-        openWhats.addTarget(self, action: #selector(openWhatsAPI), for: .touchUpInside)
         sendMessage.addTarget(self, action: #selector(openWhatsSendMessage), for: .touchUpInside)
-        
+        sendMessageSpecific.addTarget(self, action: #selector(sendWhatsNumberSpecific), for: .touchUpInside)
     }
     
-    @objc func openWhatsAPI() {
-        print("Abrir função do módulo")
+    func enterMessageAlert(_ title: String,_ message: String,_ placeholder: String, _ sendNumberSpecific: Bool) {
+        whatsapp.log(message: "Open Alert Enter Message")
+        let alert = UIAlertController(title: "\(title)", message: "\(message)", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.text = ""
+            textField.placeholder = "\(placeholder)"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Send", style: .default, handler: { [weak alert] (_) in
+            let textField = alert!.textFields![0] // Force unwrapping because we know it exists.
+            guard let message = textField.text else { return }
+            if sendNumberSpecific {
+                self.whatsapp.sendMessageWhats(message: message, vc: self)
+            } else {
+                self.whatsapp.sendMessageNumberSpecific(message: message, vc: self)
+            }
+            
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
     
     @objc func openWhatsSendMessage() {
-        print("Abrir função do módulo para enviar mensagem")
+        enterMessageAlert("Message","Enter a text","Please input something", false)
+    }
+    
+    @objc func sendWhatsNumberSpecific() {
+        enterMessageAlert("Message","Enter a text number","Ex: +5551912345678", true)
     }
     
 }
